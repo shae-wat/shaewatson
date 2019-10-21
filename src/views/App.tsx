ff839e1d90097e7ee24e34357282e4360c249248
shwatson-sofi <shwatson@sofi.org>
2019-10-21 09:46:37 -0700

    start using styled-components

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from '../images/logo.svg';
-import './App.css';
+
+const AppMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+
+  // non-prod CSS guardrails
+  ${() => {
+    if (process.env.NODE_ENV !== 'production') {
+      /* Accessibility: All imgs must have an alt attribute,
+       * see https://webaim.org/techniques/alttext/
+       */
+      return `
+        img:not([alt]) {
+          border: 5px dashed red !important;
+        }
+      `;
+    } else {
+      return ``;
+    }
+  }}
+`;
+
+const AppHeader = styled.header`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  > * {
+    margin-right: 16px;
+  }
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppMain>
+      <AppHeader>
+        <img src={logo} height={40} alt="logo" />
         <p>shaewatson.com</p>
-      </header>
-    </div>
+      </AppHeader>
+    </AppMain>
   );
 };
 

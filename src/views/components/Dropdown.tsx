f884f0fb088b58b7840516adfb24687ed25e0be2
shwatson-sofi <shwatson@sofi.org>
2019-11-26 15:59:02 -0800

    init

--- src/views/components/Dropdown.tsx
+++ src/views/components/Dropdown.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Dropdown: React.FC = () => {
-  return <div>Hello Dropdown!</div>;
-};
+enum DropdownOption {
+  HIGHEST_RATED,
+  NEWEST_FIRST,
+  MOST_REACTIONS,
+  MOST_DISAGREED,
+}
+
+interface DropdownState {
+  open: boolean;
+}
+
+const StyledDropdown = styled.div`
+  display: block;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  width: 250px;
+  border: 2px solid #ddd;
+  border-radius: 4px;
+  padding: 12px;
+`;
+
+class Dropdown extends React.Component<{}, DropdownState> {
+  state = { open: false, selectedOption: DropdownOption };
+
+  render() {
+    return <StyledDropdown>Hello Dropdown!</StyledDropdown>;
+  }
+}
 
 export default Dropdown;

e8f452072802ccd0ef900957fa6f7b9e181aeb89
shwatson-sofi <shwatson@sofi.org>
2019-12-05 18:30:36 -0800

    show open icon only when closed

--- src/views/components/Dropdown.tsx
+++ src/views/components/Dropdown.tsx
@@ -15,39 +15,13 @@ interface DropdownOption {
   value: DropdownOptionKey;
 }
 
-const StyledDropdownContainer = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-
-  cursor: pointer;
-  width: 100%;
-  max-width: ${column(3) + gutter(4)}px;
-
-  &:after {
-    position: absolute;
-    right: ${gutter(1)}px;
-    top: ${rhythm(1 / 2) + 1}px;
-    ${FontCSS}
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 28px;
-    color: ${Colors.white};
-
-    content: '<>';
-    -webkit-transform: rotate(90deg);
-    -moz-transform: rotate(90deg);
-    -ms-transform: rotate(90deg);
-    transform: rotate(90deg);
-  }
-`;
-
 const StyledSelect = styled.div`
-  width: 100%;
-  padding: ${rhythm(1 / 2)}px ${gutter(1)}px;
+  position: relative;
+  width: ${column(3) + gutter(4)}px;
+  cursor: pointer;
 
-  color: transparent;
-  text-shadow: 0 0 0 ${Colors.white};
+  color: ${Colors.background};
+  background-color: ${Colors.white};
   border: 2px solid #ddd;
   border-radius: 4px;
 
@@ -55,10 +29,31 @@ const StyledSelect = styled.div`
   font-weight: 500;
   font-size: 16px;
   line-height: 24px;
+
+  ${({ open }: { open: boolean }) => {
+    return !open
+      ? `&:after {
+          position: absolute;
+          right: ${gutter(1)}px;
+          top: ${rhythm(1 / 2) + 1}px;
+          ${FontCSS}
+          font-weight: 500;
+          font-size: 14px;
+          line-height: 28px;
+          color: ${Colors.cardBackground};
+
+          content: '<>';
+          -webkit-transform: rotate(90deg);
+          -moz-transform: rotate(90deg);
+          -ms-transform: rotate(90deg);
+          transform: rotate(90deg);
+        }`
+      : '';
+  }}
 `;
 
 const StyledOption = styled.div`
-  width: 100%;
+  flex: 1;
   padding: ${rhythm(1 / 4)}px ${gutter(1 / 4)}px;
 
   &:hover {
@@ -78,35 +73,34 @@ interface DropdownState {
 export class Dropdown extends React.Component<DropdownProps, DropdownState> {
   state = { open: false, selectedOption: DropdownOptionKey.HIGHEST_RATED };
 
-  getOptionByKey = (key: DropdownOptionKey) => {
+  getOptionByKey(key: DropdownOptionKey) {
     const { options } = this.props;
     console.log('options', options);
     switch (key) {
       default:
         return options[0];
     }
-  };
+  }
 
   render() {
     const { options } = this.props;
 
     const selected = this.getOptionByKey(this.state.selectedOption);
     return (
-      <StyledDropdownContainer>
-        <StyledSelect
-          onMouseEnter={() => this.setState({ open: true })}
-          onMouseLeave={() => this.setState({ open: false })}>
-          {this.state.open ? (
-            _.map(options, (option: DropdownOption) => {
-              return (
-                <StyledOption key={option.value}>{option.label}</StyledOption>
-              );
-            })
-          ) : (
-            <StyledOption key={selected.value}>{selected.label}</StyledOption>
-          )}
-        </StyledSelect>
-      </StyledDropdownContainer>
+      <StyledSelect
+        open={this.state.open}
+        onMouseEnter={() => this.setState({ open: true })}
+        onMouseLeave={() => this.setState({ open: false })}>
+        {this.state.open ? (
+          _.map(options, (option: DropdownOption) => {
+            return (
+              <StyledOption key={option.value}>{option.label}</StyledOption>
+            );
+          })
+        ) : (
+          <StyledOption key={selected.value}>{selected.label}</StyledOption>
+        )}
+      </StyledSelect>
     );
   }
 }
